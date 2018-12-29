e02b26910c287294f95b0df5a9afad415de601c0
Adsun <fengxiang@jfshare.com>
2018-12-29 18:03:06 +0800

    c

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ axios.defaults.withCredentials = true
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.BASE_API, // api 的 base_url
-  timeout: 5000 // 请求超时时间
+  timeout: 15000 // 请求超时时间
 })
 
 // request拦截器
